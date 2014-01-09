0834eaff06429f8d3bcc7f0dd7d5752c1aa02273
liu <liumeng64@126.com>
2014-01-09 20:42:48 +0800

    wrong unique_ptr array

--- a.cpp
+++ a.cpp
@@ -6,18 +6,31 @@ using namespace std;
 
 int main(){
     
-    int *lt=new int[5]{1,3};
-       
-for(int i=0;i<5;i++)
-        cout<<lt[i]<<endl; 
 
-    string * gs=new string[10]{"a","b","c",string(10,'o')};
-for(int i=0;i<10;i++)
-        cout<<gs[i]<<endl; 
- 
- delete [] lt;
+    int *t=new int[10]{1,2,3};
+    unique_ptr<int[]> up(t);
 
- delete [] gs;
+      for(int i=0;i<10;i++){
+
+    	 up[i]=i;
+    }
+    
+    for(int i=0;i<10;i++){
+
+    	cout<<t[i]<<endl;
+    }
+    auto pp=up.release();
+    if(pp==t)
+    	cout<<"it is true"<<endl;
+  //  delete [] t;
+     
+     up=nullptr;
+
+    for(int i=0;i<10;i++){
+
+    	cout<<t[i]<<endl;
+    }
+    
 
 
 }
